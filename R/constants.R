e9669f07d83b52056126b758d54b1ac9dfc8bf32
texagjosh <texagjosh@gmail.com>
2017-10-18 08:55:57 -0500

    Update constants.R

--- R/constants.R
+++ R/constants.R
@@ -14,14 +14,24 @@ kHeaders <- list(
     'Referer' = 'http://stats.nba.com/%referer%/',
     'User-Agent' = paste('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5)',
                          'AppleWebKit/537.36 (KHTML, like Gecko)',
-                         'Chrome/57.0.2987.133 Safari/537.36')
+                         'Chrome/57.0.2987.133 Safari/537.36'),
+    'Accept-Encoding' = "gzip, deflate",
+    'Accept' = "*/*",
+    'Connection' = "keep-alive",
+    'Cache-Control' = "no-cache",
+    'Origin' = "http://stats.nba.com"
   ),
   'NBA.Synergy' = list(
     'Accept-Language' = 'en-US,en;q=0.8,af;q=0.6',
     'Referer' = 'http://stats.nba.com/%referer%/',
     'User-Agent' = paste('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5)',
                          'AppleWebKit/537.36 (KHTML, like Gecko)',
-                         'Chrome/57.0.2987.133 Safari/537.36')
+                         'Chrome/57.0.2987.133 Safari/537.36'),
+    'Accept-Encoding' = "gzip, deflate",
+    'Accept' = "*/*",
+    'Connection' = "keep-alive",
+    'Cache-Control' = "no-cache",
+    'Origin' = "http://stats.nba.com"
   ),
   'BRef' = list(
     'Accept-Language' = 'en-US,en;q=0.8,af;q=0.6',
